f2203528db4bcc37f33a948f52f75770da587e5d
Katherine Rosenfeld <krosenfeld@idmod.org>
2024-01-17 07:37:03 +0000

    add sqlLite example

---
+++ SIRSingleNode/sqlLite/benchmark_sirsinglenode.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+SEED=42
+RANDOM=$SEED
+N_RUN=10
+
+run_model_small () {
+    times=()
+    for i in $( seq 1 $N_RUN )
+    do
+        startt=`date +%s%N`
+        python tlc.py
+        endt=`date +%s%N`
+        times+=(`expr $endt - $startt`)
+    done
+    readarray -t sorted < <(printf '%s\n' "${times[@]}" | sort)
+    echo -n "sqlLite SIRSingleNode-small (ms): "
+    echo "${sorted[(`expr $N_RUN / 2 + $N_RUN % 2`)]} * 0.000001" | bc
+}
+
+# save the current working directory so we can revert back to it later
+pushd .
+# change the current working directory to the one with this file in it
+cd "$(dirname "$0")"
+run_model_small
+# and then revert back to the original working directory
+popd
